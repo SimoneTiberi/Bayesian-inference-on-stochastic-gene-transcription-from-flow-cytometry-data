b5d868a03349f76c71dbf85cd3099481ec7c85cb
Simone Tiberi <simone.tiberi@uzh.ch>
2018-06-12 10:47:48 +0200

    Update Plot hypermean densities.R

--- R code/Plot hypermean densities.R
+++ R code/Plot hypermean densities.R
@@ -9,6 +9,7 @@ S.   = burn_in/thin + 1
 end. = R/thin # total number of iterations of the MCMC
 
 HYPER_MODE[[1]] = mcmc.hyper[[1]][S.:end.,]
+HYPER_TAU[[1]]  = mcmc.hyper[[2]][S.:end.,]
 
 # load the posterior output of the Tet_10 condition
 load(...)
@@ -16,6 +17,7 @@ S.   = burn_in/thin + 1
 end. = R/thin # total number of iterations of the MCMC
 
 HYPER_MODE[[2]] = mcmc.hyper[[1]][S.:end.,]
+HYPER_TAU[[2]]  =  mcmc.hyper[[2]][S.:end.,]
 
 ################################################################################################################
 # Posterior densities for the exponential of the hypermean parameters:
@@ -26,7 +28,6 @@ cols = 1:2
 # alpha_0
 plot(NULL, 
      main = expression(tilde(alpha)[0]),
-     #main = expression(e^{mu[1]}),
      xlim = c(-5,200), ylim = c(0, 0.06),  
      xlab = "", ylab = "", cex.main = 2, cex.axis = 2, cex.lab = 1)
 for(i in 1:2){
@@ -37,7 +38,6 @@ for(i in 1:2){
 # alpha_1
 plot(NULL, 
      main = expression(tilde(alpha)[1]),
-     #main = expression(e^{mu[2]}),
      xlim = c(-1000,2*10^4), ylim = c(0, 0.0003),
      xlab = "", ylab = "", cex.main = 2, cex.axis = 2, cex.lab = 1)
 for(i in 1:2){
@@ -47,7 +47,6 @@ for(i in 1:2){
 # k_1
 plot(NULL, 
      main = expression(tilde(k)[1]),
-     #main = expression(e^{mu[3]}),
      xlim = c(-0.1, 1), ylim = c(0,8),
      xlab = "", ylab = "", cex.main = 2, cex.axis = 2, cex.lab = 1)
 for(i in 1:2){
@@ -57,7 +56,6 @@ for(i in 1:2){
 # k_0
 plot(NULL, 
      main = expression(tilde(k)[0]),
-     #main = expression(e^{mu[4]}),
      xlim = c(1.5, 12), ylim = c(0, 0.8),
      xlab = "", ylab = "", cex.main = 2, cex.axis = 2, cex.lab = 1)
 
@@ -67,10 +65,54 @@ for(i in 1:2){
 
 # kappa
 plot(NULL,  main = expression(kappa),
-     #main = expression(e^{mu[5]}),
      xlim = c(-10, 100), ylim = c(0, 0.15),
      xlab = "", ylab = "", cex.main = 2, cex.axis = 2, cex.lab = 1)
 
 for(i in 1:2){
   lines(density(exp(HYPER_MODE[[i]][,5]), n =2*10^4, adjust = 1), col = cols[i],  lty = i, lwd = 2 )
+}
+
+################################################################################################################
+# Posterior densities for the hyper-precision parameter tau:
+################################################################################################################
+par(mfrow = c(3,2))
+
+# alpha_0
+plot(NULL, main = expression(tau[1]),
+     ylim = c(0,1.5), xlim = c(0, 6),
+     xlab = "", ylab = "", cex.main = 2, cex.axis = 1.5, cex.lab = 1)
+for(i in 1:2){
+  lines(density((HYPER_TAU[[i]][,1]),  n = 10^4), col = cols[i], lty = i, lwd = 2 )
+}
+
+# alpha_1
+plot(NULL, main = expression(tau[2]),
+     xlim = c(0,10^3), ylim = c(0, 0.012),     
+     xlab = "", ylab = "", cex.main = 2, cex.axis = 1.5, cex.lab = 1)
+for(i in 1:2){
+  lines(density((HYPER_TAU[[i]][,2]), n = 10^4), col = cols[i], lty = i, lwd = 2 )
+}
+
+# k_1
+plot(NULL, main = expression(tau[3]),
+     xlim = c(0,100), ylim = c(0, 0.08),     
+     xlab = "", ylab = "", cex.main = 2, cex.axis = 1.5, cex.lab = 1)
+for(i in 1:2){  
+  lines(density((HYPER_TAU[[i]][,3]), n = 10^4), col = cols[i], lty = i, lwd = 2 )
+}
+
+# k_0
+plot(NULL, main = expression(tau[4]),
+     xlim = c(-5, 200), ylim  = c(0,0.03),     
+     xlab = "", ylab = "", cex.main = 2, cex.axis = 1.5, cex.lab = 1)
+for(i in 1:2){
+  lines(density((HYPER_TAU[[i]][,4]), n = 10^4), col = cols[i], lty = i, lwd = 2 )
+}
+
+# kappa
+plot(NULL, main = expression(tau[5]),
+     xlim = c(-10, 300), ylim = c(0, 0.018),     
+     xlab = "", ylab = "", cex.main = 2, cex.axis = 1.5, cex.lab = 1)
+for(i in 1:2){
+  lines(density((HYPER_TAU[[i]][,5]), n = 10^4), col = cols[i], lty = i, lwd = 2 )
 }
